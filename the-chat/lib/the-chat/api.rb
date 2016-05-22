9fe8bb0077f98a04d3b6bd7ad54b2598ea6253b7
Michael Lutsiuk <michael.lutsiuk@gmail.com>
2016-05-22 14:33:11 +0000

    Registering

--- the-chat/lib/the-chat/api.rb
+++ the-chat/lib/the-chat/api.rb
@@ -11,27 +11,48 @@ module TheChat
       end
     end
 
-    helpers do
-      def current_user
-        @current_user ||= User.first(name: auth.username)
+    namespace :me do
+      helpers do
+        def current_user
+          @current_user ||= User.first(name: auth.username)
+        end
+
+        def auth
+          @auth ||= Rack::Auth::Basic::Request.new(env)
+        end
       end
 
-      def auth
-        @auth ||= Rack::Auth::Basic::Request.new(env)
+      http_basic do |username, password|
+        User.authorized?(username, password)
+      end
+
+      get :info do
+        { name: current_user.name }
+      end
+
+      get :all do
+        User.all.map do |user|
+          {name: user.name}
+        end
       end
     end
 
-    http_basic do |username, password|
-      User.authorized?(username, password)
-    end
 
-    get :me do
-      { name: current_user.name }
+    params do
+      requires :name, type: String
+      requires :password, type: String
     end
+    post :register do
+      user = User.new 'name' => params[:name]
+      user.password = params[:password]
+      user.save
 
-    get :all do
-      User.all.map do |user|
-        {name: user.name}
+      if user.persisted?
+        status 201
+        { status: 'ok' }
+      else
+        status 400
+        { error: "Username #{user.name.inspect} has already been taken" }
       end
     end
   end
--- the-chat/test/the-chat/api_test.rb
+++ the-chat/test/the-chat/api_test.rb
@@ -26,7 +26,7 @@ class TheChat::APITest < ApiTest
   end
 
   def test_unauthorized_by_default
-    get '/me' do |response|
+    get '/me/info' do |response|
       assert_predicate response, :unauthorized?
     end
   end
@@ -34,7 +34,7 @@ class TheChat::APITest < ApiTest
   def test_me
     basic_authorize username, pass
 
-    get '/me' do |response|
+    get '/me/info' do |response|
       assert_predicate response, :ok?
       assert_equal username, response.json['name']
     end
@@ -43,8 +43,26 @@ class TheChat::APITest < ApiTest
   def test_all_users
     basic_authorize username, pass
 
-    get '/all' do |response|
+    get '/me/all' do |response|
       assert_equal other_names.count + 1, response.json.count
+    end
+  end
+
+  def test_register
+    name = 'juke'
+    assert_nil TheChat::User.first('name' => name)
+    post '/register', name: name, password: name do |response|
+      assert_predicate response, :created?
+      refute_nil TheChat::User.first('name' => name)
+    end
+  end
+
+  def test_register_with_wrong_params
+    name = 'juke'
+    assert_nil TheChat::User.first('name' => name)
+    post '/register', name: name, password: nil do |response|
+      refute_predicate response, :created?
+      assert_nil TheChat::User.first('name' => name)
     end
   end
 end
